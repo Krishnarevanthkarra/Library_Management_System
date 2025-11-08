d9858ffc780b9af305817c07144c904de7bcf2cb
Bala Naga Krishna Revanth Karra <krishnarevanth.ic@gmail.com>
2025-11-08 07:39:29 +0530

    Refactor database URI to use environment variables

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
 import secrets
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
+dbusername = os.getenv('dbusername')
+dbpassword = os.getenv('dbpassword')
+dbhost = os.getenv('host')
+dbname = os.getenv('dbname')
+
 class Config:
     SECRET_KEY = secrets.token_hex(16)
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:krishna%402004@localhost/library"
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{dbusername}:{dbpassword}@{host}/{dbname}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = 'static/Images/book_covers'
