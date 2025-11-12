3d5e410cebcf07f08fe5111c1ff5c8407258b7d1
Bala Naga Krishna Revanth Karra <krishnarevanth.ic@gmail.com>
2025-11-12 23:48:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, session, redirect, url_for, sessions, request, render_template, flash
 from config import Config
 from routes import *
-from models import db
+from models import *
 app = Flask(__name__)
 app.config.from_object(Config)
 app.register_blueprint(login_bp, url_prefix='/')
@@ -13,6 +13,11 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
+    branches = ['Computer Science', 'Information Technology',
+            'Data Science', 'AIML', 'Mechanical', 'Civil']
+    for branch in branches:
+        db.session.add(models.Branch(branch_name=branch))
+        db.session.commit()
 
 @app.before_request
 def check_login():
