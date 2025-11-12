aab450a2721df019b54f4aa03bf0dde15af0b6cb
Bala Naga Krishna Revanth Karra <krishnarevanth.ic@gmail.com>
2025-11-12 23:20:07 +0530

    Update author_model.py

--- models/author_model.py
+++ models/author_model.py
@@ -3,7 +3,7 @@ from . import db
 class Author(db.Model):
     __tablename__ = 'Authors'
     author_id = db.Column(db.Integer, primary_key=True)
-    author_name = db.Column(db.String(50))
-    about_author = db.Column(db.String(500))
+    author_name = db.Column(db.Text)
+    about_author = db.Column(db.Text)
 
     books = db.relationship('Book', backref='author')
